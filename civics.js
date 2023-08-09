04d98bea5bae52a04a611808a05c712d6133c844
TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>
2023-08-09 00:09:49 -0500

    Update civics.js

--- civics.js
+++ civics.js
@@ -1,4 +1,6 @@
 function pageRedirect() {
-        window.location.replace("https://www.tutorialrepublic.com/");
+        window.location.replace("https://tyreeeddings.github.io/ogcc/hp/hp.html");
     }      
     setTimeout("pageRedirect()", 6200);
+
+ 
